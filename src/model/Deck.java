17d75419f2edd9e45327cb051b23bba253e7ec21
jssison <jssison5@up.edu.ph>
2026-04-26 21:56:02 +0800

    feat: added Deck class

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -1,5 +1,47 @@
 package model;
 
-public class Deck {
+//imports
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Deck {
+	private final List<Card> cards = new ArrayList<>();
+	
+	//constructor
+	public Deck() {
+		this.build();
+	}
+	
+	//build deck
+	//remove one queen to make sure one queen has no pair
+	private void build() {
+		for (Card.Suit suit : Card.Suit.values()) {
+			for (Card.Rank rank : Card.Rank.values()) {
+				if (rank == Card.Rank.QUEEN && suit == Card.Suit.HEARTS) {
+					//skip Queen of Hearts 
+					continue;
+				}
+				cards.add(new Card(rank, suit));
+			}
+		}
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public void dealTo(List<Player> players) {
+		int i = 0;
+		
+		//deal cards to each player
+		for (Card card : cards) {
+			players.get(i % players.size()).addCard(card);
+		}
+	}
+	
+	//getter for deck size
+	public int size() {
+		return cards.size();
+	}
 }
